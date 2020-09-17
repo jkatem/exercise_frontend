768e4874f15b247a2f74bc83edc5c94400552198
jkatem <jenmiao@gmail.com>
2020-09-17 15:35:26 -0700

    GET fetch function working

--- src/exercise.js
+++ src/exercise.js
@@ -1,5 +1,76 @@
 class Exercise {
-    constructor() {
-        
+    constructor(exercise, exerciseAttributes) {
+        this.id = exercise.id
+        this.name = exerciseAttributes.name
+        this.description = exerciseAttributes.description
+        this.duration = exerciseAttributes.duration
+        this.muscle = exerciseAttributes.muscle
+        Exercise.push(this)
+        console.log(this);
     }
 }
+
+    // I want user's inputs to be able to show up on the dom
+    // renderExercise() {
+    //     return `
+    //             <div class="col-md-f">
+    //                 <div class="card mb-4 shadow-sm">
+    //                     <div class="card-body">
+    //                         <h5 class="card-name">${this.name}</h5>
+    //                         <p class="card-text">${this.description}</p>
+    //                         <div class="d-flex justify-content-between align-items-center">
+    //                         <div class="btn-group">
+    //                         <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+    //                         <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+    //                         </div>
+    //                         // <small class="text-muted">Category: ${this.category.name}</small>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //     `
+    // }
+
+
+
+
+
+
+    // renderExerciseCard() {
+    //     return `
+    //         <div class="col-md-f">
+    //             <div class="card mb-4 shadow-sm">
+    //                 <div class="card-body">
+    //                     <h5 class="card-name">${this.name}</h5>
+    //                     <p class="card-text">${this.description}</p>
+    //                     <div class="d-flex justify-content-between align-items-center">
+    //                     <div class="btn-group">
+    //                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+    //                     <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+    //                     </div>
+    //                     <small class="text-muted">Category: ${this.category.name}</small>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+            
+    //     `
+    // }
+
+//     function render(exercise) {
+//             const exerciseMarkup = `
+//                 <div data-id=${exercise.id}>
+//                     <h3>${exercise.attributes.name}</h3>
+//                     <p>${exercise.attributes.description}</p>
+//                     <p>${exercise.attributes.duration}</p>
+//                     <button data-id=${exercise.id}>edit</button>
+//                 </div>
+//                 <br><br>`;
+        
+//             document.querySelector('#exercise-container').innerHTML += exerciseMarkup;
+        
+//         }
+
+// }
+
+// Exercise.all = [];
--- src/index.js
+++ src/index.js
@@ -2,25 +2,53 @@ const endPoint = "http://localhost:3000/api/v1/exercises"
 
 document.addEventListener('DOMContentLoaded', () => {
     getExercises()
+
+    // let createExerciseForm = document.querySelector("create-exercise-form")
+
+    // createExerciseForm.addEventListener('submit', (e) => createFormHandler(e))
+
+    // const exerciseContainer = document.querySelector('#exercise-container')
+    // exerciseContainer.addEventListener('click', e => {
+    //     console.log('clicked');
+    // });
 });
 
 
 function getExercises() {
     fetch(endPoint)
     .then(resp => resp.json())
-    .then(exercises => {
-        exercises.data.forEach(exercise => {
-            const exerciseMarkup = `
+    .then(json => renderExercises(json))
+}
+
+function renderExercises(exercises) {
+    const main = document.querySelector('main')
+    
+    exercises.data.forEach(exercise => {
+        const exerciseMarkup = `
             <div data-id=${exercise.id}>
                 <h3>${exercise.attributes.name}</h3>
                 <p>${exercise.attributes.description}</p>
                 <p>${exercise.attributes.duration}</p>
-                <p>${exercise.attributes.muscle.legs}
             </div> 
             <br><br>`;
 
-            document.querySelector('#exercise-container').innerHTML += exerciseMarkup
-
-        })
+        const h2 = document.createElement('h2')
+        h2.innerHTML = exerciseMarkup
+        main.appendChild(h2)
     })
 }
+
+
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const nameInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const durationInput = document.querySelector('#input-duration').value
+
+    const muscleId = parseInt(document.querySelector('#muscle').value)
+    postFetch(nameInput, descriptionInput, durationInput, muscleId)
+}
+
+
+
