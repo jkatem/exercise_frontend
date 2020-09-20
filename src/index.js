523c6be3dcf1a61e28db4311d26c436de1d0cbf2
jkatem <jenmiao@gmail.com>
2020-09-20 15:19:06 -0700

    create removeExercise function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ const endPoint = "http://localhost:3000/api/v1/exercises"
 
 document.addEventListener('DOMContentLoaded', () => {
     getExercises()
-
+    // event listener and handler for create exercise form 
     const createExerciseForm = document.querySelector("#create-exercise-form")
     createExerciseForm.addEventListener("submit", (e) => createFormHandler(e))
     
 });
 
-
 function getExercises() {
-    // debugger
+    // var message, x;
+    // message = document.getElementById();
+    // message.innerHTML = ""; 
+    // x = document.getElementById("main").value;
     fetch(endPoint)
     .then(resp => resp.json())
     .then(exercise => {
@@ -19,9 +21,12 @@ function getExercises() {
             newExercise.renderExercise()
         })
     })
+    // .catch(err) 
 }
 
 
+
+// create form function
 function createFormHandler(e) {
     
     e.preventDefault()
@@ -33,10 +38,12 @@ function createFormHandler(e) {
     postFetch(nameInput, descriptionInput, durationInput, muscle_id)
 }
 
+//create create new exercise function
+
 function postFetch(name, description, duration, muscle_id) {
     
     const bodyData = {name, description, duration, muscle_id}
-// debugger
+
     fetch(endPoint, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -46,8 +53,28 @@ function postFetch(name, description, duration, muscle_id) {
     .then(exercise => {
         console.log(exercise);
         const exerciseData = exercise.data
-// debugger
+
         let newExercise = new Exercise(exerciseData)
         newExercise.renderExercise()
     }
 )}
+
+// deleting an exercise
+// /api/v1/exercises/:id
+function removeExercise(e) {
+    e.preventDefault();
+    let execId = e.target.id[3];
+    fetch(`http://localhost:3000/api/v1/exercises/${e.target.id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+    .then(function(resp){
+        if (resp.status == 204)
+            location.reload();
+        else
+            console.log(resp.status)
+    })
+}
